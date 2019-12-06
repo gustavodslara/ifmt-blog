28ff5917000444f06514fe6e0774b2d6fe5188c8
Gustavo Lara <gustavo.silvalara@outlook.com>
2019-12-06 15:55:42 -0400

    add index e match posts

---
+++ src/main/java/br/gus/blog/ifmt/ifmtblog/repository/MatchNodes.java
@@ -0,0 +1,25 @@
+package br.gus.blog.ifmt.ifmtblog.repository;
+
+import br.gus.blog.ifmt.ifmtblog.model.Post;
+import org.neo4j.driver.v1.Record;
+import org.neo4j.driver.v1.Session;
+import org.neo4j.driver.v1.StatementResult;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MatchNodes {
+    public List<Post> getPosts() {
+        Session session = GraphCon.getSession();
+        StatementResult result = session.run("MATCH (n:Post) RETURN n");
+        List<Post> listaPosts = new ArrayList<>();
+        while (result.hasNext()) {
+            Record record = result.next();
+            Post p = new Post();
+            p.setTitle(record.get("title").asString());
+            p.setContent(record.get("content").asString());
+        }
+        session.close();
+        return listaPosts;
+    }
+}
