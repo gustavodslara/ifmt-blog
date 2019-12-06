3f8728cd51e34e2303925f3402229f6d3b644a90
Gustavo Lara <gustavo.silvalara@outlook.com>
2019-12-06 09:30:34 -0400

    spring initialzr

---
+++ src/main/java/br/gus/proj/blog/ifmt/ifmtblog/IfmtblogApplication.java
@@ -0,0 +1,13 @@
+package br.gus.proj.blog.ifmt.ifmtblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfmtblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfmtblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gus/proj/blog/ifmt/ifmtblog/IfmtblogApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gus.proj.blog.ifmt.ifmtblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfmtblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
