4625c06c204cb86e3ab5237f8d19c3df492c4a30
gustavo-silvalara <gustavo.silvalara@outlook.com>
2019-12-07 02:01:15 -0400

    visualizar o post completo e footer

--- src/main/java/br/gus/blog/ifmt/ifmtblog/controller/ViewPostController.java
+++ src/main/java/br/gus/blog/ifmt/ifmtblog/controller/ViewPostController.java
@@ -5,17 +5,18 @@ import br.gus.blog.ifmt.ifmtblog.repository.MatchNodes;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @Controller
-public class IndexController {
+public class ViewPostController {
 
-    @GetMapping("/")
-    public String createPost(Model model) {
+    @GetMapping("/visualizar")
+    public String createPost(@RequestParam(name = "title", required = false, defaultValue = "") String title, Model model) {
         MatchNodes mC = new MatchNodes();
-        List<Post> listaPosts = mC.findAll();
-        model.addAttribute("listaPosts", listaPosts);
-        return "index";
+        Post post = mC.getPost(title);
+        model.addAttribute("post", post);
+        return "post";
     }
 }
--- src/main/java/br/gus/blog/ifmt/ifmtblog/repository/MatchNodes.java
+++ src/main/java/br/gus/blog/ifmt/ifmtblog/repository/MatchNodes.java
@@ -9,9 +9,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MatchNodes {
-    public List<Post> getPosts() {
+    public List<Post> findAll() {
         Session session = GraphCon.getSession();
-        StatementResult result = session.run("MATCH (n:Post) RETURN ID(n) as id, n.title as title, n.content as content");
+        StatementResult result = session.run("MATCH (n:Post) RETURN id(n) as id, n.title as title, n.content as content ORDER BY id(n)");
         List<Post> listaPosts = new ArrayList<>();
         while (result.hasNext()) {
             Record record = result.next();
@@ -23,5 +23,22 @@ public class MatchNodes {
         }
         session.close();
         return listaPosts;
+    }
+
+    public Post getPost(String title) {
+        Session session = GraphCon.getSession();
+        StatementResult result = session.run("MATCH (n:Post) WHERE n.title = ? RETURN ID(n) as id, n.title as title, n.content as content"
+        .replace("?", "'"+title+"'"));
+        Post post = new Post();
+        while (result.hasNext()) {
+            Record record = result.next();
+            Post p = new Post();
+            p.setId(record.get("id").asLong());
+            p.setTitle(record.get("title").asString());
+            p.setContent(record.get("content").asString());
+            post = p;
+        }
+        session.close();
+        return post;
     }
 }
